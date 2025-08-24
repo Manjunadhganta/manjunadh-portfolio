aea2d9cd0e64a2555a159dcb835cad7409c7df19
Manjunadhganta <gantamanjunadhreddy@gmail.com>
2025-08-24 12:20:30 +0530

    small chang in 3d-marquee adjusted css

--- src/components/ui/3d-marquee.tsx
+++ src/components/ui/3d-marquee.tsx
@@ -23,7 +23,7 @@ export const ThreeDMarquee = ({
             style={{
               transform: "rotateX(55deg) rotateY(0deg) rotateZ(-45deg)",
             }}
-            className="relative top-130 right-[50%] grid size-full origin-top-left grid-cols-4 gap-8 transform-3d">
+            className="relative top-150 right-[50%] grid size-full origin-top-left grid-cols-4 gap-8 transform-3d">
             {chunks.map((subarray, colIndex) => (
               <motion.div
                 animate={{ y: colIndex % 2 === 0 ? 100 : -100 }}
